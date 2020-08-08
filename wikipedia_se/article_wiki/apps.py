a8dd3f309cfcb6eccf382b5908baf74e9a803533
PhilippeMitch <philippemitch91@gmail.com>
2020-08-08 11:28:52 +0700

    update submodule

--- wikipedia_se/article_wiki/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArticleWikiConfig(AppConfig):
-    name = 'article_wiki'
--- wikipedia_se/titre_wiki/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TitreWikiConfig(AppConfig):
-    name = 'titre_wiki'
